3a08a775860f97861e27b47d7199e8fd185055f0
bkolodiy <bkolodiy@binariks.com>
2024-11-07 16:56:37 +0200

    Fix and update chat interface + add new request for users for chat

--- src/app/services/user/user.service.ts
+++ src/app/services/user/user.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { CookieService } from 'ngx-cookie-service';
 import { Observable, tap } from 'rxjs';
-import { IUser } from '../../shared/interfaces/user.interface';
+import { IUser, IUsersForChat } from '../../shared/interfaces/user.interface';
 import { environment } from '../../../environments/environment';
 
 @Injectable({
@@ -24,8 +24,9 @@ export class UserService {
     );
   }
 
-  getUsers(): Observable<IUser[]> {
-    return this.http.get<IUser[]>(`${this.#pathUrl}user/all`);
+
+  getUsersForChat(): Observable<IUsersForChat[]> {
+    return this.http.get<IUsersForChat[]>(`${this.#pathUrl}user/forChat`);
   }
 
   setUserCookie(user: IUser) {
